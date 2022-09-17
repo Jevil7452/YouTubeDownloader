316c2d9ecf9a7724a5548f666820918dad057917
Admin <Admin@DESKTOP-TB8F7LC>
2022-09-17 22:41:02 +0200

    Dodaj pliki projektów.

---
+++ YouTubeDownloader/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace YouTubeDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(254, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse for location";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Location: ";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 59);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(254, 70);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "File format";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(11, 45);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(49, 19);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "MP4";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(11, 22);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(49, 19);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "MP3";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 162);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(254, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Start downloading";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.DetectUrls = false;
+            this.richTextBox1.Location = new System.Drawing.Point(272, 30);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(321, 155);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(272, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Download status";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "URL to download";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(116, 136);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 23);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.InitialDirectory = "%userprofile%\\desktop";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(607, 196);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "YouTube Downloader";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private GroupBox groupBox1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private Button button2;
+        private RichTextBox richTextBox1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private FolderBrowserDialog folderBrowserDialog1;
+    }
+}
---
+++ YouTubeDownloader/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Text;
+using System.Windows.Forms;
+
+namespace YouTubeDownloader
+{
+    public partial class Form1 : Form
+    {
+        string Path = "";
+        string fileType = "";
+        string URL = "";
+        BackgroundWorker Processer = new BackgroundWorker();
+        public Form1()
+        {
+            InitializeComponent();
+            Processer.WorkerReportsProgress = true;
+            Processer.WorkerSupportsCancellation = true;
+            Processer.ProgressChanged += Processer_ProgressChanged;
+            Processer.DoWork += Processer_DoWork;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(folderBrowserDialog1.ShowDialog() ==  DialogResult.OK)
+            { 
+                Path = folderBrowserDialog1.SelectedPath;
+                label1.Text = "Location: " + folderBrowserDialog1.SelectedPath;
+            }
+        }
+            private void Processer_DoWork(object sender, DoWorkEventArgs e)
+        {
+            StreamReader StandardOutput = e.Argument as StreamReader;
+            string data = StandardOutput.ReadLine();
+            while (data != null)
+            {
+                Processer.ReportProgress(0, data);
+                data = StandardOutput.ReadLine();
+            }
+        }
+        private void Processer_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            string data = e.UserState as string;
+            if (data != null)
+                richTextBox1.Text += data + "\r\n";
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            fileType = " -x --audio-format mp3 ";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            fileType = " -f mp4 ";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            URL = textBox1.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Path == "")
+            {
+                MessageBox.Show("Path cannot be empty");
+            }
+            else if (fileType == "")
+            {
+                MessageBox.Show("Select a file type");
+            }
+            else if (URL == "")
+            {
+                MessageBox.Show("URL cannot be empty");
+            }
+            else
+            {
+                using (Process process = new Process())
+                {
+                    process.StartInfo.FileName = "yt-dlp.exe";
+                    process.StartInfo.Arguments = "-P " + Path + fileType + URL;
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.CreateNoWindow = true;
+                    process.StartInfo.RedirectStandardOutput = true;
+                    process.Start();
+                    Processer.RunWorkerAsync(process.StandardOutput);
+                }
+
+            }
+        }
+
+        private void richTextBox1_TextChanged_1(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionStart = richTextBox1.Text.Length;
+            richTextBox1.ScrollToCaret();
+        }
+    }
+}
